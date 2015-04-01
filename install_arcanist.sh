3eb20b7b0bfa8039320e618a219f8fe8e39c4c3e
sumit jain <sumit.jain@snapdeal.com>
2015-04-01 15:58:33 +0530

    fixed phab directory and bashrc echo

--- install_arcanist.sh
+++ install_arcanist.sh
@@ -4,13 +4,15 @@
 set -e
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y php5
-PHAB_DIR='~/phabricator'
+PHAB_DIR=$HOME/phabricator
+echo "creating directory $PHAB_DIR"
 mkdir -p $PHAB_DIR
 git clone https://github.com/phacility/libphutil.git $PHAB_DIR/libphutil
 git clone https://github.com/phacility/arcanist.git $PHAB_DIR/arcanist
 
-echo 'export PATH="$PATH:~/phabricator/arcanist/bin/"' >> ~/.bashrc
-echo 'export PATH="$PATH:~/phabricator/arcanist/bin/"' >> ~/.profile
+echo "export PATH=\"\$PATH:$PHAB_DIR/arcanist/bin/\"" >> ~/.bashrc
+echo "source $PHAB_DIR/arcanist/resources/shell/bash-completion" >> ~/.bashrc
+echo "export PATH=\"\$PATH:$PHAB_DIR/arcanist/bin/\"" >> ~/.profile
 
 source ~/.bashrc
 arc install-certificate
